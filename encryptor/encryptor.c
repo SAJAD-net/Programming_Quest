06d063ca5dfb92c05d14b41d9e529f998a9d6134
SAJAD <71703544+SAJAD-net@users.noreply.github.com>
2021-08-17 20:45:08 +0430

    Replaced some vulnerable functions with new functions

--- encryptor/encryptor.c
+++ encryptor/encryptor.c
@@ -2,18 +2,18 @@
 #define MAX_FILE_SIZE (1024*1024)
 int main(int argc[],char *argv[]){
 
-	FILE *fpntr;
+	FILE *fptr;
 	int i;
 	int pass=argv[1];
 	char data[MAX_FILE_SIZE];
-	fpntr=fopen(argv[2],"rb");
-	while (!feof(fpntr))
-		data[i++] = getc(fpntr);
-	fclose(fpntr);
-	fpntr=fopen(argv[2],"wb");
+	fptr=fopen(argv[2],"rb");
+	while (!feof(fptr))
+		data[i++] = fgetc(fptr);
+	fclose(fptr);
+	fptr=fopen(argv[2],"wb");
 	for (int j=0; j<i-1; j++)
-		putc((data[j] ^ pass),fpntr);
-	fclose(fpntr);	
+		fputc((data[j] ^ pass),fptr);
+	fclose(fptr);	
 	return 0;
 }
 
