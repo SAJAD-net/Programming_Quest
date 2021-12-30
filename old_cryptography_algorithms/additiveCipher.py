0ceaf700da8d4def8a7ca6aeacd581df8e6e90f2
SAJADnet <sajadsajad.sch@gmail.com>
2021-12-30 11:55:19 +0330

    the old_Cryptography_Algorithms renamed to old_cryptography_algorithms

---
+++ old_cryptography_algorithms/additiveCipher.py
@@ -0,0 +1,31 @@
+from sys import argv
+
+alpha = list("abcdefghijklmnopqrstuvwxyz")
+
+def encryptor(key, plain):
+    cipher=""
+    
+    for ch in plain:
+        ind = alpha.index(ch.lower())
+        dex = lambda x:x+key-26 if x+key >= 26 else x+key
+        nc = alpha[dex(ind)]
+        cipher+=nc
+    print(cipher)
+
+def decryptor(key, cipher):
+    plain=""
+    for ch in cipher:
+        ind = alpha.index(ch.lower())
+        dex = lambda x:x-key+26 if x-key < 0 else x-key
+        nc = alpha[dex(ind)]
+        plain+=nc
+    print(plain)
+
+if len(argv) > 3:
+    key = int(argv[2])
+    if argv[1] == "-e":
+        plain = argv[3]
+        encryptor(key, plain)
+    elif argv[1] == "-d":
+        cipher = argv[3]
+        decryptor(key, cipher)        
