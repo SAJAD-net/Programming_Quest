58478faa45113cb62f5b32d1ced98bd45385e384
SAJAD-net <sajadsajad.sch@gmail.com>
2022-02-22 23:50:45 +0330

    Some Bugs were resolved

--- jcat/jcat.jl
+++ jcat/jcat.jl
@@ -10,21 +10,20 @@ end
 #Reads the file and displays it's context
 function reader()
     #checks the user inputs
-    try
-	for arg in ARGS
-	    if length(arg) > 2 
-                global fname = arg
-    	    end
-	end
-    catch
-        help()
+    for arg in ARGS
+    	if length(arg) > 2
+		global fname = arg
+    	else
+	    help()
+    	end
     end
+
     #Reads the file 	 
     try
     	global fname
     	open(fname) do f
     	    while ! eof(f)
-    	        line = readline(f) #Reads the file 
+    	        line = readline(f) #Reads the file line by line
 	    	if "-s" in ARGS
 		    time = parse(Float64,ARGS[2]) #User time input
 	   	    sleep(time) #Waits for [time] in second 
@@ -32,9 +31,9 @@ function reader()
 		println(line)	
             end
     	end
-	catch
-    	    println("can't open this file ~ $fname")
-	end
+    catch
+	help()
+    end
 end
 
 reader()
