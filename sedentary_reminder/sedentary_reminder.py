7157247a20fb2046b5d0bef46ca87a849e33c2f2
SAJADnet <sajadsajad.sch@gmail.com>
2023-03-08 03:27:55 +0330

    adding argparse

--- sedentary_reminder/sedentary_reminder.py
+++ sedentary_reminder/sedentary_reminder.py
@@ -1,6 +1,8 @@
-#Sedentary_reminder
+#!/usr/bin/env python3
 
-from sys import argv
+
+import argparse
+import sys
 from os import getpid, kill
 from signal import SIGUSR1
 from time import sleep
@@ -9,25 +11,53 @@ from tkinter import messagebox
 from threading import Thread
 from playsound import playsound
 
+
 def tkshow(label):
+    """ Displays a simple tkinter window """
+
     now = datetime.now().strftime("%H:%m:%S")
-    messagebox.showinfo( "Alarm", f"Time : {now}\n{label}")
+    messagebox.showinfo("Alarm", f"Time : {now}\n{label}")
     kill(getpid(), SIGUSR1)
 
-def chtime(time_type, user_time):
-    if time_type == "-m":
-        user_time *= 60   
-    elif time_type == "-h":
-        user_time*=(60*60)
-    return user_time
 
-def tasks():
-    if len(argv) > 3:
-        time_type = argv[1]
-        user_time = int(argv[2]) 
-        right_time = chtime(time_type, user_time)
-        label = argv[4] 
-        sleep(right_time)
+def chtime(time_type, delay):
+    """ Calculate the delay time in seconds """
+    if time_type == "m":
+        delay *= 60
+    elif time_type == "h":
+        delay *= (60 * 60)
+    return delay
+
+
+# Initialize the command line arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-m", "--minute", help="one minute")
+ap.add_argument("-H", "--hour", help="one hour")
+ap.add_argument("-l", "--label", help="label")
+
+args = vars(ap.parse_args())
+
+
+def main():
+    if len(args) == 3:
+        if delay := args["minute"]:
+            time_type = "m"
+        elif delay := args["hour"]:
+            time_type = "h"
+        if args["label"]:
+            label = args["label"]
+        else:
+            ap.print_help()
+            sys.exit()
+
+        fin_time = chtime(time_type, int(delay))
+        sleep(fin_time)
         Thread(target=tkshow, args=(label,)).start()
         playsound("alarm.mp3")
-tasks()
+
+    else:
+        ap.print_help()
+        sys.exit()
+
+
+main()
