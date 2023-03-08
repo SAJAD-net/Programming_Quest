a39fe752f8cd8b8f09a3a4f7be310f7d05559e29
SAJADnet <sajadsajad.sch@gmail.com>
2023-03-08 04:27:03 +0330

    some cleaning

--- passcwd/passcwd.py
+++ passcwd/passcwd.py
@@ -1,21 +1,27 @@
+#!/usr/bin/env python3
+
 from random import seed, choice
 from sys import argv
 seed()
 
+
 def make(length):
-    alpha = list("abcdefghijklmnopqrstuvwxyz"\
-    +"ABCDEFGHIJKLMNOPQRSTUVWXYZ"\
-    +"0123456789"+"!@#$&_")
+    alpha = list("abcdefghijklmnopqrstuvwxyz" +
+                 "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                 "0123456789" + "!@#$&_")
+
     passwd = ""
-        
+
     for _ in range(length):
         passwd += choice(alpha)
     print(passwd)
 
+
 if len(argv) > 1:
-    length = int(argv[1])        
-    make(length)
+    LENGTH = int(argv[1])
+    make(LENGTH)
 else:
-    print("error, password length not found !")
-         
-        
+    print("Error, password length not found !")
+    print("Usage:\n\tpython3 passcwd.py [LENGTH]")
+
+
