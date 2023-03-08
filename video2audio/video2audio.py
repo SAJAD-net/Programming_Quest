7e16bfa0103e481d052da3fce1ca0942faa0833f
SAJADnet <sajadsajad.sch@gmail.com>
2023-03-08 04:31:39 +0330

    some cleaning

--- video2audio/video2audio.py
+++ video2audio/video2audio.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from sys import argv
 from moviepy.editor import VideoFileClip
 
@@ -5,9 +7,11 @@ from moviepy.editor import VideoFileClip
 def _help():
     print("usage : python3 video2audio.py [path_to_video_file]")
 
-def aspliter(path): 
-        video = VideoFileClip(path)
-        name = path.split("/").split(".")[-2]
-        video.audio.write_audiofile(fr"{name}.mp3")
+
+def aspliter(path):
+    video = VideoFileClip(path)
+    name = path.split("/").split(".")[-2]
+    video.audio.write_audiofile(fr"{name}.mp3")
+
 
 aspliter(argv[1]) if len(argv) > 1 else _help()
